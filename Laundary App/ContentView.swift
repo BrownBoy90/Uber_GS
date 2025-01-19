2dab864c9a34bb081c2ce01235077149573d0923
Gurnoor Singh <gotohellgth0001@gmail.com>
2025-01-19 17:58:58 +0530

    Changed ZStack to NavigationStack

--- Laundary App/ContentView.swift
+++ Laundary App/ContentView.swift
@@ -2,7 +2,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
+        NavigationStack {
             ZStack {
                 ScrollView(showsIndicators: false) {
                     // Uber =>Always Fixed
